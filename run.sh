509584c10ee15464f52d98bea51ecf58a4c835c2
johnnyopao <jandresopao@gmail.com>
2020-01-03 12:34:55 -0500

    unset AWS_PROFILE when picking default profile

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
 AWS_PROFILE="$AWS_PROFILE" _awsp_prompt
 
-export AWS_PROFILE="$(cat ~/.awsp)"
+selected_profile="$(cat ~/.awsp)"
+
+if [ -z "$selected_profile" ]
+then
+  unset AWS_PROFILE
+else
+  export AWS_PROFILE="$selected_profile"
+fi
